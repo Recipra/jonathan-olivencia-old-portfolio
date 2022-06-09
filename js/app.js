444306c12fb6c49411ac9a8b33415dd2fe83c779
recipra <jmo0425@outlook.com>
2022-06-09 12:03:33 -0400

    comment out work on carousel work import, add favicon

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 import { projectData } from "./project-data.js"
 
+// const innerCarousel = document.getElementById('carousel-inner')
+
+// let projectCarousel = projectData.map(project => {
+//   `
+//   <div class="carousel-inner">
+//     <div class="carousel-item active" class="d-block w-100" alt="tictactoe">
+//       <div class="carousel-image" style="background-image: url('${project.image}')"></div>
+//       <div class="carousel-caption d-none d-md-block">
+//         <h5>${project.title}</h5>
+//         <p>${project.description}</p>
+//       </div>
+//     </div>
+//   <div>
+//   `
+// })
+
+// console.log(projectCarousel)
+// innerCarousel.innerHTML = projectCarousel
+
 const aboutButton = document.getElementById('about-button')
 const contactButton = document.getElementById('contact-button')
 const resumeButton = document.getElementById('resume-button')
--- js/project-data.js
+++ js/project-data.js
@@ -2,9 +2,23 @@ let projectData = [
   {
     title: 'Tic-Tac-Toe',
     deployed: 'https://jonathan-olivencia-ga-tic-tac-toe-game.netlify.app/',
-    image: '/assets/tictactoe screenshot.jpg',
+    image: '/assets/tictactoe-screenshot.png',
     github: 'https://github.com/Recipra/jonathan-olivencia-portfolio',
     description: 'A simple tic-tac-toe game'
+  },
+  {
+    title: 'Brown Horses',
+    deployed: '',
+    image: '/assets/horses1.jpg',
+    github: '',
+    description: 'Two awesome horses'
+  },
+  {
+    title: 'Black Horse',
+    deployed: '',
+    image: '/assets/horses2.jpg',
+    github: '',
+    description: 'One horse with a DUMPY'
   }
 ]
 
