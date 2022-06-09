1c22d85c5c8141180ba336c580280e1752243da4
recipra <jmo0425@outlook.com>
2022-06-09 14:12:05 -0400

    add contact icon hover interaction

--- js/app.js
+++ js/app.js
@@ -25,6 +25,12 @@ const aboutButton = document.getElementById('about-button')
 const contactButton = document.getElementById('contact-button')
 const resumeButton = document.getElementById('resume-button')
 const workButton = document.getElementById('work-button')
+const linkedInLogo = document.getElementById('linkedin-logo')
+const gitHubLogo = document.getElementById('github-logo')
+const emailLogo = document.getElementById('email-logo')
+const linkedInText = document.getElementById('linkedin-text')
+const gitHubText = document.getElementById('github-text')
+const emailText = document.getElementById('email-text')
 
 aboutButton.addEventListener('click', event => {
   scrollToSection(event)
@@ -45,4 +51,28 @@ workButton.addEventListener('click', event => {
 function scrollToSection(event) {
   let elementId = event.target.id.replace('-button', '')
   document.getElementById(elementId).scrollIntoView(true)
+}
+
+linkedInLogo.onmouseover = function() {
+  linkedInText.style.color = 'purple'
+}
+
+linkedInLogo.onmouseout = function() {
+  linkedInText.style.color = 'transparent'
+}
+
+gitHubLogo.onmouseover = function() {
+  gitHubText.style.color = 'purple'
+}
+
+gitHubLogo.onmouseout = function() {
+  gitHubText.style.color = 'transparent'
+}
+
+emailLogo.onmouseover = function() {
+  emailText.style.color = 'purple'
+}
+
+emailLogo.onmouseout = function() {
+  emailText.style.color = 'transparent'
 }
